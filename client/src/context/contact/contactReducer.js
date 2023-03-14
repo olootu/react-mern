32916570037144e8a139b1a468bfca420d529a03
Ola Ogunyemi <olootu6831@yahoo.co.uk>
2023-03-14 16:34:46 +0000

    forms and form edit

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -1,5 +1,31 @@
-import {createContext} from 'react';
+import {
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    FILTER_CONTACTS,
+    CLEAR_FILTER
+} from '../types';
 
-const ContactReducer = createContext();
+export default (state, action ) => {
+    switch(action.type) {
+        case ADD_CONTACT:
+            return {
+                ...state,
+                contacts: [...state.contacts, action.payload]
+            };
+        case DELETE_CONTACT:
+            return {
+                ...state,
+                contacts: state.contacts.filter(contact => contact.id !== action.payload)
+            }
+        default:
+            return state;
+    }
+}
 
-export default ContactReducer;
+// import {createContext} from 'react';
+
+// const ContactReducer = createContext();
+
+// export default ContactReducer;
--- client/src/context/contact/contactState.js
+++ client/src/context/contact/contactState.js
@@ -1,5 +1,5 @@
 import React, {useReducer} from "react";
-import uuid from 'uuid';
+import {v4 as uuid} from 'uuid';
 import contactContext from "./contactContext";
 import contactReducer from  "./contactReducer";
 import {
@@ -44,8 +44,18 @@ const ContactState = props => {
     const [state, dispatch] = useReducer(contactReducer, initialState);
 
     // Add Contact
+    const addContact = contact => {
+        contact.id = uuid();
+        dispatch({type: ADD_CONTACT, payload: contact});
+
+    }
 
     // Delete Contact
+
+    const deleteContact = id => {
+        dispatch({type: DELETE_CONTACT, payload: id});
+
+    }
 
     // Set Current Contact
 
@@ -59,7 +69,11 @@ const ContactState = props => {
 
     return(
         <ContactContext.Provider
-        value={{contacts: state.contacts}}
+        value={{
+            contacts: state.contacts,
+            addContact,
+            deleteContact
+        }}
         >
             { props.children}
         </ContactContext.Provider>
